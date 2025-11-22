e4791017d46f8bcff12ebd4e01200bd4e72c01dc
Tawheeb-AbdulAlim <tawheebabdul@gmail.com>
2025-11-22 18:50:10 +0300

    first commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,7 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 16F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(182, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "wellcome in the hospital stystem";
             // 
             // Form1
             // 
@@ -36,13 +48,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Highlight;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
