d206d271f9aac430135c3fe180aec1c8808cc15a
nora <lyan44936@gmail.com>
2025-11-24 20:00:03 +0300

    patient form

---
+++ AddEditeVisitForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hospital_management_system
+{
+    public partial class AddEditeVisitForm : Form
+    {
+        public AddEditeVisitForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddEditeVisitForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
