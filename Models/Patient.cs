42506e8570347554114f40517f30e04675011977
sadeq123422 <sadeqalhdal50@gmail.com>
2025-11-26 23:59:41 +0300

    اضافة مجلد services and Model

---
+++ Models/Patient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_management_system.Models
+{
+    class Patient
+    {
+    }
+}
