3c912ee5af5fc66da0e252e11706f4bb2e832284
sadeq123422 <sadeqalhdal50@gmail.com>
2025-11-28 23:14:27 +0300

    تعديل فورم AddEditVisit واضافة خصائص للكلاسات user ,visit

--- Models/User.cs
+++ Models/User.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using System.ComponentModel.DataAnnotations;
 
 namespace Hospital_management_system.Models
 {
     class User
     {
+        //[Key]
+        public int UserId { get; set; }
+
+        //[Required]
+        //[Foreignkey("Eemployee")]
+        public int EmployeeId { get; set; }
+
+        //[Required]
+       // [StringLength(50)]
+        public string Username{ get; set; }
+
+        //[Required]
+       // [StringLength(50)]
+        public string Role { get; set; }
+
+
+        //[Required]
+        //[StringLength(30)]
+        public string PasswordHash { get; set; }
+
     }
+
+
+
 }
--- Models/Visit.cs
+++ Models/Visit.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//using System.ComponentModel.DataAnnotations;
 namespace Hospital_management_system.Models
 {
     class Visit
     {
+       //[Key]
+   public int VisitId { get; set; }
+
+
+        //[Required]
+        //[Foreignkey("Patient")]
+        public int PatientId { get; set; }
+
+        //[Required]
+        //[Foreignkey("Employee")]
+        public int DoctorId { get; set; }
+
+        //[Required]
+        public DateTime VisitDate { get; set; }
+
+        // [StringLength(50)]
+        public string VisitType { get; set; }
+
+        // [StringLength(50)]
+        //الادوية الحالية
+        public string CurrentMedication { get; set; }
+
+
+        public string Notes { get; set; }
+
+
     }
 }
