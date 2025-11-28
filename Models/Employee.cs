e3fb284bed7ce0cbdfe7c12a45268651812eb0a7
nora <lyan44936@gmail.com>
2025-11-28 21:16:35 +0300

    adding doctor and employee properties

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Hospital_management_system.Models
 {
-    class Employee
+    public class Employee
     {
+        public int EmployeeId { get; set; }     // Primary Key
+        public string FullName { get; set; }
+        public string Role { get; set; }        // مثل: Receptionist, Admin, Accountant
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime HireDate { get; set; }
+        public decimal Salary { get; set; }
     }
 }
