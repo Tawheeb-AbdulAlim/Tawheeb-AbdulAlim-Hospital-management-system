bc05719af281384b8194a0454af2d3ca4e9e87b2
Tawheeb-AbdulAlim <tawheebabdul@gmail.com>
2025-11-29 00:20:42 +0300

    instal package EF6  and created a hospitaldbcontext

--- Program.cs
+++ Program.cs
@@ -16,16 +16,11 @@ namespace Hospital_management_system
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
+
             Application.Run(new Form2
                 ());
 
             
-=======
-            Application.Run(new Form1());
-
-           // Application.Run(new SignIn());
->>>>>>> 1527fb834494bb77314da6b6909a56dc73f79c74
 
 
 
